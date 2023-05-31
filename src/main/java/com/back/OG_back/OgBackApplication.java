27cc589e9b6f412934e00d6855b53d1379a9fc9a
BRUNEL Robin <robin.brunel@ext.soprasteria.com>
2023-05-31 11:03:02 +0200

    Premier commit

---
+++ src/main/java/com/back/OG_back/OgBackApplication.java
@@ -0,0 +1,13 @@
+package com.back.OG_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/OG_back/OgBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.OG_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
